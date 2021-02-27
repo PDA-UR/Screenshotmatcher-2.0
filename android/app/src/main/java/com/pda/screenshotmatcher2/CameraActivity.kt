9dce2c1d4f79bde77644dee08e3601e0e1fa6baf
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-02-27 21:29:48 +0100

    🚧Added CameraActivity as first test

---
+++ android/app/src/main/java/com/pda/screenshotmatcher2/CameraActivity.kt
@@ -0,0 +1,74 @@
+package com.pda.screenshotmatcher2
+
+import android.Manifest
+import android.app.Activity
+import android.content.Context
+import android.content.pm.PackageManager
+import android.hardware.Camera
+
+import android.os.Bundle
+import android.widget.FrameLayout
+import androidx.core.app.ActivityCompat.requestPermissions
+import androidx.core.content.ContextCompat
+
+
+class CameraActivity : Activity() {
+
+    private var mCamera: Camera? = null
+    private var mPreview: CameraPreview? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+        requestPerm()
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance()
+
+        mPreview = mCamera?.let {
+            // Create our Preview view
+            CameraPreview(this, it)
+        }
+
+        // Set the Preview view as the content of our activity.
+        mPreview?.also {
+            val preview: FrameLayout = findViewById(R.id.camera_preview)
+            preview.addView(it)
+        }
+    }
+
+    private fun requestPerm() {
+        when {
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED -> {
+                // You can use the API that requires the permission.
+            }
+
+            else -> {
+                // You can directly ask for the permission.
+                // The registered ActivityResultCallback gets the result of this request.
+                requestPermissions(this,
+                    arrayOf(Manifest.permission.CAMERA),
+                    12)
+            }
+        }
+    }
+
+
+    /** Check if this device has a camera */
+    private fun checkCameraHardware(context: Context): Boolean {
+        return context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+    }
+
+    /** A safe way to get an instance of the Camera object. */
+    fun getCameraInstance(): Camera? {
+        return try {
+            Camera.open() // attempt to get a Camera instance
+        } catch (e: Exception) {
+            // Camera is not available (in use or does not exist)
+            null // returns null if camera is unavailable
+        }
+    }
+}
