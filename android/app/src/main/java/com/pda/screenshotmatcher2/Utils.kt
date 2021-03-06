1c9c7114540a221f051dd9e4d846356c27551299
Snackya <13784757+Snackya@users.noreply.github.com>
2021-03-06 03:51:50 +0100

    App: Refactor
    - due to Android's scoped storage introduced in Q and enforced in R being an absolute abomination, the app now only uses internal storage instead of saving to /Pictures
    - removed all the unnecessary global variables and nullables in the result activity
    - outsourced some more code to helper files
    - TODO: more cleanup

--- android/app/src/main/java/com/pda/screenshotmatcher2/Utils.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/Utils.kt
@@ -4,22 +4,29 @@ import android.Manifest
 import android.app.Activity
 import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
+import android.os.ParcelFileDescriptor
+import android.provider.DocumentsContract
 import android.provider.MediaStore
 import android.util.Base64
 import android.util.Log
 import androidx.core.app.ActivityCompat
+import androidx.core.app.ActivityCompat.startActivityForResult
 import java.io.File
+import java.io.FileDescriptor
 import java.io.FileOutputStream
 import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
 
-const val APP_DIRECTORY : String = "/ScreenshotMatcher/"
+const val IMG_DIR : String = "Pictures/ScreenshotMatcher"
 
 private val PERMISSIONS = arrayOf<String>(
     Manifest.permission.READ_EXTERNAL_STORAGE,
@@ -31,6 +38,11 @@ private val PERMISSIONS = arrayOf<String>(
 fun getDataDir(context: Context): String? {
     return context.packageManager
         .getPackageInfo(context.packageName, 0).applicationInfo.dataDir
+}
+
+fun getDateString() : String{
+    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss", Locale.getDefault())
+    return sdf.format(Date())
 }
 
 fun verifyPermissions(activity: Activity?) {
@@ -73,36 +85,31 @@ fun saveB64ToInternalFile(b64String: String, context: Context){
 }
 
 @Suppress("DEPRECATION")
-fun saveFileToExternalDir(b64String: String, context: Context, dirName: String = "Pictures/"): String {
+fun saveFileToExternalDir(bitmap: Bitmap, context: Context, isFullScreenshot : Boolean = false){
     val out : OutputStream
-    val dir : String = Environment.DIRECTORY_PICTURES + APP_DIRECTORY + dirName
-    val filename = "croppedScreenshot.jpg"
-    Log.d("saving", "Dir name: $dir")
+    val filename = when(isFullScreenshot){
+        false -> getDateString() + ".jpg"
+        true -> getDateString() + "_full.jpg"
+    }
+    if(!File(IMG_DIR).exists()) {
+        File(IMG_DIR).mkdir()
+    }
 
-    if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
+    out = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
         val resolver = context.contentResolver
         val contentValues = ContentValues()
         contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
         contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-        contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, dir)
+        contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, IMG_DIR)
         val imageUri : Uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)!!
-        out = resolver.openOutputStream(imageUri)!!
+        resolver.openOutputStream(imageUri)!!
     }
     else{
-        val imageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
-        val file = File(dir)
-        if(!file.exists()){
-            file.mkdir()
-        }
-        val image = File(dir, filename)
-        out = FileOutputStream(image)
+        val image = File(IMG_DIR, filename)
+        FileOutputStream(image)
     }
 
-    val byteArray = Base64.decode(b64String, Base64.DEFAULT)
-    val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
     bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
     out.flush()
     out.close()
-
-    return dir
 }
