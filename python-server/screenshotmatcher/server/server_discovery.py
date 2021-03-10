60ab1f0ee6b2d4091ff8a940f9b3c2fe579ffc8e
Snackya <13784757+Snackya@users.noreply.github.com>
2021-03-10 16:46:19 +0100

    Cleanup

--- python-server/screenshotmatcher/server/server_discovery.py
+++ python-server/screenshotmatcher/server/server_discovery.py
@@ -1,27 +1,7 @@
 import socket
 from common.config import Config
 
-UDP_PORT = Config.PORT_DISCOVERY
-HTTP_PORT = Config.PORT
 interrupt_discovery_flag = False
-
-# start in thread to prevent blocking from socket.recvfrom()
-def local_network_address():
-    # https://stackoverflow.com/a/28950776
-    s_ip = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # socket for finding local network IP
-    s_ip.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)  # reuse address if already in use
-    ip = ""
-    try:
-        s_ip.connect(('10.255.255.255', 1))
-        ip = s_ip.getsockname()[0]
-    except Exception as e:
-        print("Exception caught while trying to determine server IP")
-        print(e)
-    finally:
-        s_ip.close()
-    address = ip + ":" + str(HTTP_PORT)                         # send the address of the flaks server back, not that of the socket.
-    print("UDP socket address: {}".format(address))
-    return address
 
 # start in thread to prevent blocking from socket.recvfrom()
 def start():
@@ -29,7 +9,7 @@ def start():
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # socket for discovery
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     # reuse address if already in use
-    s.bind(('0.0.0.0', UDP_PORT))
+    s.bind(('0.0.0.0', Config.PORT_DISCOVERY))
 
     while not interrupt_discovery_flag:                         # interupt by setting flag
         bytesAddressPair = s.recvfrom(1024)
