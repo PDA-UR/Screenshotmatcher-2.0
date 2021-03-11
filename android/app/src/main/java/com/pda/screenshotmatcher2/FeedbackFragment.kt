fd85e1506e9dd0d88af8f3ab439e64634a7d42aa
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-03-11 12:44:48 +0100

    ✨FeedbackFragment functionality added
    - Feedback Fragment now sends feedback to server
    - Fixed Bug with Result Activity trying to delete a cropped image file, which doesn't exist when started over the error fragment

--- android/app/src/main/java/com/pda/screenshotmatcher2/FeedbackFragment.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/FeedbackFragment.kt
@@ -1,23 +1,39 @@
 package com.pda.screenshotmatcher2
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.FrameLayout
+import android.view.inputmethod.InputMethodManager
+import android.widget.*
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 
+
+//Views
+lateinit var layout: LinearLayout
 lateinit var containerView: FrameLayout
-lateinit var backgroundDarkening: FrameLayout
+lateinit var mFragmentBackground: FrameLayout
 lateinit var mSendFeedbackButton: Button
+lateinit var mTextInputField: EditText
+
+//Server info
+lateinit var uid: String
+lateinit var url: String
 
 class FeedbackFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val bundle = this.arguments
+        if (bundle != null) {
+            uid = bundle.getString(UID_KEY, "undefined")
+            url = bundle.getString(URL_KEY, "undefined")
+        }
     }
 
     override fun onCreateView(
@@ -32,15 +48,59 @@ class FeedbackFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        backgroundDarkening = activity?.findViewById(R.id.ca_dark_background)!!
-        backgroundDarkening.setOnClickListener { removeThisFragment(true) }
-        mSendFeedbackButton = activity?.findViewById(R.id.ef_send_feedback_button)!!
-        mSendFeedbackButton.setOnClickListener { removeThisFragment(true) }
+        initViews()
+    }
+
+    fun onFeedbackPosted(){
+        removeThisFragment(true)
+        Toast.makeText(context, getText(R.string.ff_submit_success_en), Toast.LENGTH_LONG).show()
+    }
+
+    private fun dismissKeyboard() {
+        val imm =
+            context?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(mTextInputField.windowToken, 0)
+        mTextInputField.clearFocus()
+    }
+
+    private fun isKeyboardActive(): Boolean {
+        return mTextInputField.hasFocus()
+    }
+
+    private fun initViews(){
+        layout = activity?.findViewById(R.id.ff_linearLayout)!!
+        layout.setOnClickListener {
+            if (isKeyboardActive()){
+                dismissKeyboard()
+            }
+        }
+        mFragmentBackground = activity?.findViewById(R.id.ca_dark_background)!!
+        mFragmentBackground.setOnClickListener {
+            if (isKeyboardActive()){
+                dismissKeyboard()
+            } else{
+                removeThisFragment(true)
+            }
+        }
+        mTextInputField = activity?.findViewById(R.id.ff_text_input)!!
+        mSendFeedbackButton = activity?.findViewById(R.id.ff_send_feedback_button)!!
+        mSendFeedbackButton.setOnClickListener {
+            sendFeedbackToServer(this, requireActivity().applicationContext, url, uid,
+                hasResult = false,
+                hasScreenshot = false,
+                comment = getInputTextFieldText()
+            )
+            removeThisFragment(true)
+        }
+    }
+
+    private fun getInputTextFieldText(): String {
+        return mTextInputField.text.toString()
     }
 
     private fun removeThisFragment(removeBackground: Boolean = true) {
         containerView.visibility = View.INVISIBLE
-        if (removeBackground){backgroundDarkening.visibility = View.INVISIBLE}
+        if (removeBackground){mFragmentBackground.visibility = View.INVISIBLE}
         activity?.supportFragmentManager
             ?.beginTransaction()
             ?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
