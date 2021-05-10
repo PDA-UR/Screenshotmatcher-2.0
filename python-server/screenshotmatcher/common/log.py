9a547e486fde849135237288dcb8ca939dbcaaac
Andreas Schmid <andreas.schmid@ur.de>
2021-05-10 23:21:46 +0200

    backend: consistent use of single quotes

--- python-server/screenshotmatcher/common/log.py
+++ python-server/screenshotmatcher/common/log.py
@@ -10,6 +10,6 @@ class Logger():
         pass
 
     def send_log(self):
-        print("sending log data:")
+        print('sending log data:')
         print(self.value_pairs)
         requests.post(url=Config.LOG_HOST, json=self.value_pairs, verify=False)
--- python-server/screenshotmatcher/server/server_discovery.py
+++ python-server/screenshotmatcher/server/server_discovery.py
@@ -6,7 +6,7 @@ interrupt_discovery_flag = False
 # start in thread to prevent blocking from socket.recvfrom()
 def start():
     payload = (
-        str(Config.HOST) + ":" + str(Config.PORT) + "|" + str(socket.gethostname())
+        str(Config.HOST) + ':' + str(Config.PORT) + '|' + str(socket.gethostname())
         ).encode('ASCII')
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)        # socket for discovery
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)     # reuse address if already in use
@@ -15,7 +15,7 @@ def start():
     while not interrupt_discovery_flag:                         # interupt by setting flag
         bytesAddressPair = s.recvfrom(1024)
         client_address = bytesAddressPair[1]
-        print("Server IP requested by Client: {}".format(client_address))
+        print('Server IP requested by Client: {}'.format(client_address))
 
         s.sendto(payload, client_address)
         
