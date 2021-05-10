dcbdfb79c4124d05b668f4b845d858dcf617fb3a
Andreas Schmid <andreas.schmid@ur.de>
2021-05-10 18:29:30 +0200

    fix indentation for main and matcher

--- python-server/screenshotmatcher/main.py
+++ python-server/screenshotmatcher/main.py
@@ -17,27 +17,27 @@ from matching.matcher import Matcher
 from server.server import Server
 
 def main():
-  # Init Server
-  server = Server()
-
-  # Init Tray
-  tray = gui.tray.Tray(Config.APP_NAME)
-
-  # Print Banner
-  print_banner( Config.APP_NAME, Config.CURRENT_VERSION, Config.IDENTIFIER )
-
-  # Create saving directory, if neccessary
-  create_results_dir()
-
-  # Start server in different thread
-  x = threading.Thread(target=server.start, args=(), daemon=True)
-  x.start()
-
-  # Start server discovery via UDP socket in different thread
-  udp_t = threading.Thread(target=discovery.start, args=(), daemon=True)
-  udp_t.start()
-
-  # Start Tray event loop
-  tray.run()
+    # Init Server
+    server = Server()
+    
+    # Init Tray
+    tray = gui.tray.Tray(Config.APP_NAME)
+    
+    # Print Banner
+    print_banner( Config.APP_NAME, Config.CURRENT_VERSION, Config.IDENTIFIER )
+    
+    # Create saving directory, if neccessary
+    create_results_dir()
+    
+    # Start server in different thread
+    x = threading.Thread(target=server.start, args=(), daemon=True)
+    x.start()
+    
+    # Start server discovery via UDP socket in different thread
+    udp_t = threading.Thread(target=discovery.start, args=(), daemon=True)
+    udp_t.start()
+    
+    # Start Tray event loop
+    tray.run()
 
 main()
