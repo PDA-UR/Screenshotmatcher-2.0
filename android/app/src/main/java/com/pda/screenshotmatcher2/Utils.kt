2905cf5f9bbd58a3b6710322734bfa4abf1930f4
Snackya <13784757+Snackya@users.noreply.github.com>
2021-05-19 05:19:02 +0200

    App: remove unused functions

--- android/app/src/main/java/com/pda/screenshotmatcher2/Utils.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/Utils.kt
@@ -31,11 +31,6 @@ private val PERMISSIONS = arrayOf<String>(
     Manifest.permission.CAMERA,
 )
 
-fun getDataDir(context: Context): String? {
-    return context.packageManager
-        .getPackageInfo(context.packageName, 0).applicationInfo.dataDir
-}
-
 fun getDateString() : String{
     val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH-mm-ss", Locale.getDefault())
     return sdf.format(Date())
@@ -65,53 +60,6 @@ fun base64ToBitmap(b64String: String) : Bitmap{
     val byteArray = Base64.decode(b64String, Base64.DEFAULT)
     val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
     return bitmap
-}
-
-fun saveB64ToInternalFile(b64String: String, context: Context){
-    val filename = System.currentTimeMillis().toString() + ".jpg"
-    val byteArray = Base64.decode(b64String, Base64.DEFAULT)
-    val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-    val targetDir = context.getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)
-    val file = File(targetDir, filename)
-    try {
-        val out = FileOutputStream(file)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
-        out.flush()
-        out.fd.sync()
-        out.close()
-    } catch (e: Exception){
-        e.printStackTrace()
-    }
-}
-
-@Suppress("DEPRECATION")
-fun saveFileToExternalDir(bitmap: Bitmap, context: Context, isFullScreenshot : Boolean = false){
-    val out : OutputStream
-    val filename = when(isFullScreenshot){
-        false -> getDateString() + ".jpg"
-        true -> getDateString() + "_full.jpg"
-    }
-    if(!File(IMG_DIR).exists()) {
-        File(IMG_DIR).mkdir()
-    }
-
-    out = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q){
-        val resolver = context.contentResolver
-        val contentValues = ContentValues()
-        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, filename)
-        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpg")
-        contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, IMG_DIR)
-        val imageUri : Uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)!!
-        resolver.openOutputStream(imageUri)!!
-    }
-    else{
-        val image = File(IMG_DIR, filename)
-        FileOutputStream(image)
-    }
-
-    bitmap.compress(Bitmap.CompressFormat.JPEG, 90, out)
-    out.flush()
-    out.close()
 }
 
 fun saveBitmapToFile(filename: File, bitmap: Bitmap){
