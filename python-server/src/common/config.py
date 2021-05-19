35518237852e7fd1dc1236a93e2511a14cc26f04
Snackya <13784757+Snackya@users.noreply.github.com>
2021-05-19 05:08:26 +0200

    Server: further cleanup

--- python-server/src/common/config.py
+++ python-server/src/common/config.py
@@ -2,14 +2,11 @@ import common.utils
 
 class Config():
     APP_NAME = 'Screenshot Matcher'
-    CURRENT_VERSION = 'v1.1.0'
     ICON_PATH = '../res/tray_icon.png'
     HOST = common.utils.get_current_ip_address()
     LOG_HOST = 'https://132.199.132.34:49051/'
     PORT = 49049
     PORT_DISCOVERY = 49050
     SERVICE_URL = 'http://{}:{}'.format(HOST, PORT)
-    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-
     DEFAULT_ALGORITHM = 'ORB'
     CURRENT_ALGORITHM = DEFAULT_ALGORITHM
