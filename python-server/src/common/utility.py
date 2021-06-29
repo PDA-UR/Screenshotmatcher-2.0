e1bd2c929ec6e40b2cab2e9e06fca85987ab5b0d
thomfischer <13784757+thomfischer@users.noreply.github.com>
2021-06-30 01:25:38 +0200

    Server: implement device permissions partially

--- python-server/src/common/utility.py
+++ python-server/src/common/utility.py
@@ -54,3 +54,72 @@ def ask_for_id():
         messagebox.showerror("Entry error", "No participant ID entered. Closing.")  
 
     return _input
+
+def is_device_allowed(current_setting, MAC_addr, device_name):
+    if current_setting == 1:    # allow all
+        return True
+    if current_setting == 2:    # block all
+        return False
+    if is_device_in_list(MAC_addr, "whitelist.txt"):
+        return True
+    if is_device_in_list(MAC_addr, "blacklist.txt"):
+        return False
+    if current_setting == 0:    # ask permission
+        allowed = request_permission_for_device(MAC_addr, device_name)
+        return allowed
+    
+
+def is_device_in_list(MAC_addr, filename):
+    if not os.path.isfile(filename):
+        return False
+
+    with open(filename, "w+") as f:
+        for line in f.readlines():
+            if line == MAC_addr:
+                return True
+        return False
+
+def add_device_to_list(MAC_addr, filename, user_response):
+    if not os.path.isfile(filename):
+        with open(filename, "w+") as f:
+            pass
+    
+    with open(filename, "a+") as f:
+        f.write(MAC_addr + "\n")
+
+    if filename == "whitelist.txt":
+        user_response = "allow"
+    elif filename == "blacklist.txt":
+        user_response = "block"
+
+def request_permission_for_device(MAC_addr, device_name):
+    user_response = ""
+    prompt_text = "The device {} is asking for permission to connect to ScreenshotMatcher.\n (MAC address: {})".format(device_name, MAC_addr)
+    ROOT = tk.Tk()
+
+    label = tk.Label(
+        ROOT,
+        text=prompt_text
+    )
+    btn_allow = tk.Button(
+        ROOT,
+        text="Allow",
+        command=add_device_to_list(MAC_addr, "whitelist.txt", user_response)
+    )
+    btn_allow_once = tk.Button(
+        ROOT,
+        text="Allow once",
+        command=set_user_response(user_response, "allow once")
+    )
+    btn_block = tk.Button(
+        ROOT,
+        text="Block",
+        command=add_device_to_list(MAC_addr, "blacklist.txt", user_response)
+    )
+
+    label.pack(side="TOP")
+    btn_allow.pack(side="LEFT")
+    btn_allow_once.pack()
+    btn_block.pack(side="RIGHT")
+
+    ROOT.mainloop()
