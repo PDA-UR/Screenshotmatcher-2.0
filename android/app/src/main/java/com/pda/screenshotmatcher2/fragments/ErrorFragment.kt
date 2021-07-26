ca1f8ea0c1583681a67678ae2d2431ec4cd8dd28
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-07-26 17:24:10 +0200

    🧹 Fragments Cleanup

--- android/app/src/main/java/com/pda/screenshotmatcher2/fragments/ErrorFragment.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/fragments/ErrorFragment.kt
@@ -19,7 +19,6 @@ import com.pda.screenshotmatcher2.activities.RESULT_ACTIVITY_REQUEST_CODE
 import com.pda.screenshotmatcher2.activities.ResultsActivity
 import com.pda.screenshotmatcher2.network.sendLog
 
-
 const val UID_KEY: String = "UID"
 const val URL_KEY: String = "URL"
 
@@ -37,22 +36,29 @@ class ErrorFragment : Fragment() {
     lateinit var url: String
     lateinit var bmp: Bitmap
 
-    private var removeForRotation: Boolean = false
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val bundle = this.arguments
         if (bundle != null) {
             uid = bundle.getString(UID_KEY, "undefined")
             url = bundle.getString(URL_KEY, "undefined")
-            if (bundle.getParcelable<Bitmap>("bmp") == null){
-                bmp = BitmapFactory.decodeResource(context?.resources,
+            bmp = if (bundle.getParcelable<Bitmap>("bmp") == null){
+                BitmapFactory.decodeResource(context?.resources,
                     R.drawable.ic_comic_characters_sad
                 )
             } else {
-                bmp = bundle.getParcelable<Bitmap>("bmp")!!
+                bundle.getParcelable("bmp")!!
             }
         }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        containerView = container as FrameLayout
+        containerView.visibility = View.VISIBLE
+        return inflater.inflate(R.layout.fragment_error, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -62,13 +68,13 @@ class ErrorFragment : Fragment() {
 
     private fun initViews() {
         mBackButton = requireView().findViewById(R.id.ef_back_button)
-        mBackButton.setOnClickListener { removeThisFragment(true) }
+        mBackButton.setOnClickListener { removeThisFragment() }
         mFeedbackButton = requireView().findViewById(R.id.ef_send_feedback_button)
         mFeedbackButton.setOnClickListener { openFeedbackFragment() }
         mFullImageButton = requireView().findViewById(R.id.ef_full_image_button)
         mFullImageButton.setOnClickListener { openResultsActivity() }
         mFragmentBackground = activity?.findViewById(R.id.ca_dark_background)!!
-        mFragmentBackground.setOnClickListener { removeThisFragment(true) }
+        mFragmentBackground.setOnClickListener { removeThisFragment() }
         mFragmentBackground.visibility = View.VISIBLE
         mErrorImageView = requireView().findViewById(R.id.errorFragmentImage)
         Glide.with(requireActivity())
@@ -88,38 +94,23 @@ class ErrorFragment : Fragment() {
     }
 
     private fun openFeedbackFragment() {
-        val feedbackFragment =
-            FeedbackFragment()
-
-        val bundle = Bundle()
-        bundle.putString(UID_KEY, uid)
-        bundle.putString(URL_KEY, url)
-        feedbackFragment.arguments = bundle
+        val feedbackFragment = FeedbackFragment()
+        feedbackFragment.arguments = Bundle().apply {
+            putString(UID_KEY, uid)
+            putString(URL_KEY, url)
+        }
 
         activity?.supportFragmentManager
             ?.beginTransaction()
             ?.replace(R.id.feedback_fragment_container_view, feedbackFragment, "feedbackFragment")
             ?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
             ?.commit()
-        removeThisFragment(false)
+        removeThisFragment()
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        containerView = container as FrameLayout
-        containerView.visibility = View.VISIBLE
-        return inflater.inflate(R.layout.fragment_error, container, false)
-    }
 
-    private fun removeThisFragment(removeBackground: Boolean = true) {
-        removeForRotation = !removeBackground
-
-        if(!removeForRotation){
-            mFragmentBackground.visibility = View.INVISIBLE
-
-        }
+    private fun removeThisFragment() {
+        mFragmentBackground.visibility = View.INVISIBLE
         containerView.visibility = View.INVISIBLE
         activity?.supportFragmentManager?.beginTransaction()?.remove(this)
             ?.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)?.commit()
@@ -129,14 +120,5 @@ class ErrorFragment : Fragment() {
         super.onStop()
         sendLog(url, requireContext())
         StudyLogger.hashMap.clear()
-    }
-
-    override fun onDestroy() {
-        if(!removeForRotation){
-            mFragmentBackground.visibility = View.INVISIBLE
-
-        }
-        containerView.visibility = View.INVISIBLE
-        super.onDestroy()
     }
 }
