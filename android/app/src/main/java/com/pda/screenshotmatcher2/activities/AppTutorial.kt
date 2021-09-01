364e00ae0e6bd7ace7e5c56c3652a0f64ebe98d3
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-09-01 14:14:05 +0200

    Haptic feedback & Permissions in AppTutorial

--- android/app/src/main/java/com/pda/screenshotmatcher2/activities/AppTutorial.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/activities/AppTutorial.kt
@@ -1,8 +1,13 @@
 package com.pda.screenshotmatcher2.activities
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.view.HapticFeedbackConstants
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.preference.PreferenceManager
 import com.github.appintro.AppIntro
@@ -19,6 +24,7 @@ class AppTutorial : AppIntro() {
             imageParallaxFactor = -1.0,
             descriptionParallaxFactor = 2.0
         ))
+
         addSlide(AnimatedIntroFragment.newInstance(R.layout.fragment_animated_intro))
 
         addSlide(AppIntroFragment.newInstance(
@@ -45,26 +51,52 @@ class AppTutorial : AppIntro() {
             descriptionColor = Color.BLACK,
             backgroundColor = Color.WHITE
         ))
-        setColorSkipButton(Color.BLACK)
+
+       askForPermissions(
+            permissions = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.INTERNET,
+                Manifest.permission.CAMERA
+            ),
+            slideNumber = 4,
+            required = true)
+        setBackArrowColor(Color.BLACK)
         setNextArrowColor(Color.BLACK)
         showStatusBar(false)
         setIndicatorColor(Color.BLACK, Color.LTGRAY)
         setColorDoneText(Color.BLACK)
+        isWizardMode = true
     }
 
-    override fun onSkipPressed(currentFragment: Fragment?) {
-        super.onSkipPressed(currentFragment)
-        savePerfAndStartCamera()
-        finish()
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED &&
+            this.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            savePerfAndStartCamera()
+        }
     }
 
-    override fun onDonePressed(currentFragment: Fragment?) {
-        super.onDonePressed(currentFragment)
-        savePerfAndStartCamera()
-        finish()
+    override fun onUserDeniedPermission(permissionName: String) {
+        // User pressed "Deny"
+        Toast.makeText(this, getString(R.string.app_intro_denied_permission), Toast.LENGTH_SHORT).show()
+    }
+    override fun onUserDisabledPermission(permissionName: String) {
+        // User pressed "Deny" + "Don't ask again" on the permission dialog
+        Toast.makeText(this, getString(R.string.app_intro_disabled_permission), Toast.LENGTH_SHORT)
+    }
+    override fun onSlideChanged(oldFragment: Fragment?, newFragment: Fragment?) {
+        super.onSlideChanged(oldFragment, newFragment)
+        window.decorView.performHapticFeedback(HapticFeedbackConstants.CONFIRM, HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING)
     }
     private fun savePerfAndStartCamera() {
+        // debug: val FIRST_RUN_KEY = "r"
         val FIRST_RUN_KEY = getString(R.string.FIRST_RUN_KEY)
+        window.decorView.performHapticFeedback(HapticFeedbackConstants.GESTURE_END, HapticFeedbackConstants.FLAG_IGNORE_GLOBAL_SETTING)
         val sp = PreferenceManager.getDefaultSharedPreferences(this)
         sp.edit().putBoolean(FIRST_RUN_KEY, false).apply()
         val intent = Intent(this, CameraActivity::class.java)
