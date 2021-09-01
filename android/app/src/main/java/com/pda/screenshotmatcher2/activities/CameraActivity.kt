973504b117eec7a09604212603c3816e5d443560
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-09-01 12:17:13 +0200

    🎬 Added greeting animation

--- android/app/src/main/java/com/pda/screenshotmatcher2/activities/CameraActivity.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/activities/CameraActivity.kt
@@ -102,8 +102,8 @@ class CameraActivity : AppCompatActivity(), SensorEventListener {
     private fun checkForFirstRun(context: Context) {
         val FIRST_RUN_KEY = getString(R.string.FIRST_RUN_KEY)
         val isFirstRun: Boolean = sp.getBoolean(FIRST_RUN_KEY, true)
-        if(isFirstRun) {
-        // {
+        //if(isFirstRun) {
+        if(true){
             val intent = Intent(context, AppTutorial::class.java)
             startActivity(intent)
             finish()
