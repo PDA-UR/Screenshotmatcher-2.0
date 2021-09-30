5a53d9e325bdd0a311accd73241a1fceaaaf86cc
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-09-30 11:23:44 +0200

    Test implementation of connection view model

--- android/app/src/main/java/com/pda/screenshotmatcher2/network/NetworkDiscovery.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/network/NetworkDiscovery.kt
@@ -49,6 +49,41 @@ fun discoverServersOnNetwork(context: Context, port: Int = 49050, message: Strin
     activity.runOnUiThread { activity.serverConnection.onServerURLsGet(serverList)}  // call function on main thread }
     return serverList
 }
+fun discoverServersOnNetwork(context: Context, port: Int = 49050, message: String = "screenshot matcher client LF server", onGet : (servers: List<Pair<String, String>>) -> Unit) : List<Pair<String, String>> {
+    val s = DatagramSocket().also {
+        it.broadcast = true
+        it.reuseAddress = true
+        it.soTimeout = 500
+        if(!it.isBound) it.bind(InetSocketAddress(port))
+    }
+    val bufS = message.toByteArray()   //send
+    val bufR = ByteArray(1024)    //receive
+    val bcAddress = getBroadcastAddress(
+        context
+    ) ?: return emptyList()
+    val packetS = DatagramPacket(bufS, bufS.size, bcAddress, port)
+    val packetR = DatagramPacket(bufR, bufR.size)
+    val serverList = mutableListOf<Pair<String, String>>()
+
+    s.send(packetS)
+    // try to get answers from every server on the LAN
+    // expected answer from server: "192.168.0.45:49049|Desktop-5QFF67"
+    for (i in 1..MAX_SERVERS){
+        try {
+            s.receive(packetR)  // receive will block here, until soTimeout gets reached
+            val payload = String(packetR.data, 0, packetR.length).split('|')
+            serverList.add(
+                Pair(PROTOCOL + payload[0], payload[1])
+            )
+        }
+        catch(e: SocketTimeoutException) {
+            break
+        }
+    }
+    s.close()
+    onGet(serverList)
+    return serverList
+}
 
 @Throws(IOException::class)
 private fun getBroadcastAddress(context : Context): InetAddress? {
