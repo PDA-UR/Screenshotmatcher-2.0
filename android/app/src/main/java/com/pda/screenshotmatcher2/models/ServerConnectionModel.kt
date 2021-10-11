1b74c942e1ef8ca17997f68105dfe94fe9f032da
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-10-11 10:58:01 +0200

    Outsourced ServerConnectionModel, livedata

--- android/app/src/main/java/com/pda/screenshotmatcher2/models/ServerConnectionModel.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/models/ServerConnectionModel.kt
@@ -1,54 +1,152 @@
 package com.pda.screenshotmatcher2.models
 
+import android.app.Application
+import android.content.Context
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.pda.screenshotmatcher2.network.discoverServersOnNetwork
+import com.pda.screenshotmatcher2.network.sendHeartbeatRequest
+
 object ServerConnectionModel {
-    private var serverUrlList: List<Pair<String, String>> = emptyList()
-    private var serverUrl: String = ""
+    var serverUrlList = MutableLiveData<List<Pair<String, String>>>(emptyList())
+    var serverUrl = MutableLiveData<String>("")
 
-    private var isConnected: Boolean = false
-    private var isDiscovering: Boolean = false
-    private var isHeartbeating: Boolean = false
+    var isConnected = MutableLiveData<Boolean>(false)
+    var isDiscovering = MutableLiveData<Boolean>(false)
+    var isHeartbeating = MutableLiveData<Boolean>(false)
 
+    private var application: Application? = null
+
+    //Handlers for discover/heartbeat thread
+    private lateinit var handlerThread: HandlerThread
+    private lateinit var looper: Looper
+    private lateinit var mHandler: Handler
+
+    private val END_ALL_THREADS: Int = 0
+    private val START_DISCOVER: Int = 1
+    private val START_HEARTBEAT: Int = 2
 
     fun setServerUrlList(serverUrlList: List<Pair<String, String>>): List<Pair<String, String>> {
-        this.serverUrlList = serverUrlList
-        return this.serverUrlList
+        this.serverUrlList.postValue(serverUrlList)
+        return this.serverUrlList.value!!
     }
 
-    fun setServerUrl(serverUrl: String): String {
-        this.serverUrl = serverUrl
-        return this.serverUrl
+    fun setServerUrl(hostname: String) {
+        serverUrlList.value?.forEach {
+            if (it.second == hostname) {
+                serverUrl.postValue(it.first)
+                onConnectionChanged(true)
+            }
+        }
+    }
+    private fun onConnectionChanged(isConnected: Boolean) {
+        isDiscovering.postValue(!isConnected)
+        isHeartbeating.postValue(isConnected)
+        this.isConnected.postValue(isConnected)
+        if (isConnected) startHeartbeatThread()
+        else {
+            serverUrl.postValue("")
+            startDiscoverThread()
+        }
     }
 
-    fun setConnected (isConnected: Boolean): Boolean {
-        this.isConnected = isConnected
-        return this.isConnected
+
+    fun start(application: Application) {
+        this.application = application
+        if (!isDiscovering.value!! && !isHeartbeating.value!!) {
+            handlerThread = HandlerThread(this.javaClass.simpleName).apply { start() }
+            looper = handlerThread.looper
+            mHandler = object : Handler(looper) {
+                override fun handleMessage(msg: Message) {
+                    when (msg.what) {
+                        END_ALL_THREADS -> {
+                            this.removeCallbacksAndMessages(null)
+                        }
+                        START_DISCOVER -> {
+                            this.removeCallbacksAndMessages(null)
+                            this.post(discoverRunnable)
+                        }
+                        START_HEARTBEAT -> {
+                            this.removeCallbacksAndMessages(null)
+                            this.post(heartbeatRunnable)
+                        }
+                    }
+                }
+            }
+            when (isConnected.value) {
+                true -> startHeartbeatThread()
+                false -> startDiscoverThread()
+            }
+        }
     }
 
-    fun setDiscovering (isDiscovering: Boolean): Boolean {
-        this.isDiscovering = isDiscovering
-        return this.isDiscovering
+    private fun startHeartbeatThread() {
+        if (handlerThread.isAlive) {
+            isHeartbeating.postValue(true)
+            mHandler.sendMessage(mHandler.obtainMessage(START_HEARTBEAT))
+        }
+    }
+    private fun startDiscoverThread() {
+        if (handlerThread.isAlive) {
+            isDiscovering.postValue(true)
+            mHandler.sendMessage(mHandler.obtainMessage(START_DISCOVER))
+        }
     }
 
-    fun setHeartbeating (isHeartbeating: Boolean): Boolean {
-        this.isHeartbeating = isHeartbeating
-        return this.isHeartbeating
+    val discoverRunnable = object : Runnable {
+        override fun run() {
+            Log.d("SCVM", "Discovering")
+            requestServerURL(application!!.applicationContext)
+            mHandler.postDelayed(this, 1000)
+        }
     }
 
-    fun getServerUrlList(): List<Pair<String, String>> {
-        return serverUrlList
+    private fun requestServerURL(context: Context) {
+        Thread {
+            serverUrlList.postValue(
+                discoverServersOnNetwork(
+                    context,
+                    49050,
+                    "",
+                    ::onServerURLsGet
+                )
+            )
+        }.start()
+    }
+    private fun updateServerUrlList(newServers: List<Pair<String, String>>) {
+        serverUrlList.postValue(newServers)
     }
 
-    fun getServerUrl(): String {
-        return serverUrl
+
+    private fun onServerURLsGet(servers: List<Pair<String, String>>) {
+        if (servers.isNotEmpty()) {
+            updateServerUrlList(servers).also {
+                // Connect to first server if currently disconnected
+                if (serverUrl.value!! == "")
+                    setServerUrl(hostname = servers[0].second)
+            }
+        }
     }
 
-    fun getDiscovering(): Boolean {
-        return isDiscovering
+    val heartbeatRunnable = object : Runnable {
+        override fun run() {
+            Log.d("SCVM", "Heartbeating")
+            if (isConnected.value!! && serverUrl.value != "") {
+                sendHeartbeatRequest(
+                    serverUrl.value,
+                    application!!.applicationContext,
+                    ::onHeartbeatFail
+                )
+            }
+            mHandler.postDelayed(this, 1000)
+        }
     }
-    fun getHeartbeating(): Boolean {
-        return isHeartbeating
-    }
-    fun getConnected(): Boolean {
-        return isConnected
+    fun onHeartbeatFail() {
+        onConnectionChanged(false)
+        Log.d("SCVM", "Failed HB")
     }
 }
