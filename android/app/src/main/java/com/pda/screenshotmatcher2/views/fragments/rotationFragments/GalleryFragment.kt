e4b7dfc433916379060448f42c2ced0c97a2ece1
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2021-12-01 11:54:34 +0100

    Documented rotation fragments

--- android/app/src/main/java/com/pda/screenshotmatcher2/views/fragments/rotationFragments/GalleryFragment.kt
+++ android/app/src/main/java/com/pda/screenshotmatcher2/views/fragments/rotationFragments/GalleryFragment.kt
@@ -7,21 +7,33 @@ import android.view.View
 import android.widget.FrameLayout
 import android.widget.GridView
 import android.widget.ImageButton
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.pda.screenshotmatcher2.R
 import com.pda.screenshotmatcher2.viewModels.GalleryViewModel
 import com.pda.screenshotmatcher2.viewHelpers.GridBaseAdapter
 import java.io.File
 
+/**
+ * [RotationFragment] displaying all old match results saved in the external app directory as a grid.
+ *
+ * @property mFragmentBackground The dark background behind the fragment
+ * @property mBackButton The back button to close the fragment
+ * @property mGridView The grid view to display the old match results
+ * @property adapter The adapter to fill the grid view
+ */
 class GalleryFragment : RotationFragment() {
 
-    //Views
     private lateinit var mFragmentBackground: FrameLayout
     private lateinit var mBackButton: ImageButton
     private lateinit var mGridView: GridView
     private lateinit var adapter: GridBaseAdapter
 
+    /**
+     * Initializes the fragment.
+     *
+     * Calls [initViews] to initialize the views
+     * Registers an observer to [GalleryViewModel.images], which invalidates the adapter when new images are available
+     */
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initViews()
@@ -29,13 +41,16 @@ class GalleryFragment : RotationFragment() {
         ViewModelProvider(requireActivity(), GalleryViewModel.Factory(requireActivity().application))
             .get(GalleryViewModel::class.java)
             .apply {
-                getImages().observe(viewLifecycleOwner, Observer {
+                getImages().observe(viewLifecycleOwner) {
                     Log.d("GF", "Refreshing")
                     if (::adapter.isInitialized) adapter.notifyDataSetInvalidated()
-            })
-        }
+                }
+            }
     }
 
+    /**
+     * Initializes all views and the adapter.
+     */
     private fun initViews() {
         mFragmentBackground = activity?.findViewById(R.id.ca_dark_background)!!
         mFragmentBackground.setOnClickListener { removeThisFragment(true) }
@@ -49,6 +64,12 @@ class GalleryFragment : RotationFragment() {
         }
     }
 
+    /**
+     * Removes this fragment to rotate it.
+     *
+     * Saves the current state of [GalleryPreviewFragment] if it is open in order to restore it after rotation.
+     *
+     */
     override fun removeThisFragmentForRotation(): ArrayList<File?>? {
         val pFrag: GalleryPreviewFragment? =
             activity?.supportFragmentManager?.findFragmentByTag(GalleryPreviewFragment::class.simpleName) as GalleryPreviewFragment?
@@ -64,6 +85,11 @@ class GalleryFragment : RotationFragment() {
         return super.removeThisFragmentForRotation()
     }
 
+    /**
+     * Removes this fragment.
+     *
+     * @param removeBackground Whether to remove the dark background behind the fragment or not
+     */
     override fun removeThisFragment(removeBackground: Boolean) {
         containerView.visibility = View.INVISIBLE
         ca.window.decorView.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY_RELEASE)
