e9bf3c315be5ad65951530c63ccfdcf591e0d2ca
AlexW00 <55558407+AlexW00@users.noreply.github.com>
2022-01-21 12:39:46 +0100

    remove

--- android/app/src/main/java/com/pda/screenshotmatcher2/logger/Logger.kt
+++
@@ -1,6 +0,0 @@
-package com.pda.screenshotmatcher2.logger
-
-// singleton
-object StudyLogger {
-    var hashMap: HashMap<Any?, Any?> = HashMap()
-}
--- python-server/Build-Windows.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-pip install pyinstaller
-pip install -r .\requirements.txt
-
-pyinstaller ./src/main.py --window --onedir --name Screenshotmatcher --icon="./res/tray_icon.ico"
